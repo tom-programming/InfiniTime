53bfba5c0fe8c0fa1190a9ded21ff6b492fb2e5a
Your <debian@debian.domain>
2023-01-13 02:10:14 +0200

    debug commit to be reverted

--- src/heartratetask/HeartRateTask.h
+++ src/heartratetask/HeartRateTask.h
@@ -64,6 +64,7 @@ SetLPROn, SetLPROff
       uint8_t *buffer; //[5];
       uint8_t buffer_index;
       uint8_t errorCode;
+      uint8_t counter_rounds;
 #ifndef SIM
       bool measuringLP = false;
 #endif
